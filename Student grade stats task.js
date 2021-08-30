0f109a7dd2a1272e988dddbac3c34d8de9fc68bd
Giladoron <gilad042@gmail.com>
2021-08-30 21:09:01 +0300

    Finding the median grade

--- Student grade stats task.js
+++ Student grade stats task.js
@@ -8,3 +8,12 @@ let range = max - min;
 
 
 console.log(range)
+
+const median = arr => {
+    const mid = Math.floor(arr.length / 2),
+      nums = [...arr].sort((a, b) => a - b);
+    return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+  }
+
+  console.log(median(grades));
+  
